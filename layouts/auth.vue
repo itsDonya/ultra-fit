3889853131d50d79bd57581a366d8dbd97165a0a
Donya <thisis2nya@gmail.com>
2024-10-10 18:04:53 +0330

    feat(auth): responsive Register page

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -7,7 +7,7 @@
 
     <!-- form -->
     <div
-      class="w-[480px] p-8 bg-white/10 backdrop-blur-md flex items-center justify-center rounded-3xl shadow-2xl z-10">
+      class="w-11/12 sm:w-[480px] p-5 sm:p-8 bg-white/10 backdrop-blur-md flex items-center justify-center rounded-3xl shadow-2xl z-10">
       <slot></slot>
     </div>
   </article>
--- pages/register.vue
+++ pages/register.vue
@@ -1,24 +1,25 @@
 <template>
   <form
     @keydown.enter="registerUser"
-    class="w-full flex flex-col items-center justify-center gap-8">
-    <h1 class="text-xl text-neutral-200">ایجاد حساب کاربری</h1>
+    class="w-full flex flex-col items-center justify-center gap-4 sm:gap-8">
+    <h1 class="sm:text-xl text-neutral-200">ایجاد حساب کاربری</h1>
 
-    <div class="w-full flex flex-col items-center justify-start gap-3">
-      <div class="w-full flex items-center justify-center gap-2">
+    <div
+      class="w-full flex flex-col items-center justify-start gap-1.5 sm:gap-3">
+      <div class="w-full flex items-center justify-center gap-1.5 sm:gap-2">
         <!-- first name -->
         <input
           type="text"
           placeholder="نام"
           v-model="data.firstName"
-          class="w-full h-12 p-4 text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-xl-tw transition-all duration-200" />
+          class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
 
         <!-- lastname -->
         <input
           type="text"
           placeholder="نام خانوادگی"
           v-model="data.lastName"
-          class="w-full h-12 p-4 text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-xl-tw transition-all duration-200" />
+          class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
       </div>
 
       <!-- user name -->
@@ -26,22 +27,22 @@
         type="text"
         placeholder="نام کاربری"
         v-model="data.username"
-        class="w-full h-12 p-4 text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-xl-tw transition-all duration-200" />
+        class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
 
-      <div class="w-full flex items-center justify-center gap-2">
+      <div class="w-full flex items-center justify-center gap-1.5 sm:gap-2">
         <!-- password -->
         <input
           type="password"
           placeholder="رمز عبور"
           v-model="data.password"
-          class="w-full h-12 p-4 text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-xl-tw transition-all duration-200" />
+          class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
 
         <!-- confirm password -->
         <input
           type="password"
           placeholder="تکرار رمز عبور"
           v-model="confirmPassword"
-          class="w-full h-12 p-4 text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-xl-tw transition-all duration-200" />
+          class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
       </div>
 
       <p
@@ -62,13 +63,13 @@
         :loading="loading"
         :disabled="!validData || loading"
         @click.prevent="registerUser"
-        class="w-full h-12">
-        <span class="text-white">ثبت نام</span>
+        class="w-full h-9 sm:h-12 rounded-lg sm:rounded-xl">
+        <span class="text-xs sm:text-base">ثبت نام</span>
       </v-btn>
 
       <!-- login -->
       <nuxt-link to="/login">
-        <p class="text-sm text-neutral-300">
+        <p class="text-xs sm:text-sm text-neutral-300">
           حساب کاربری دارید؟
           <span class="text-secondary underline">وارد شوید</span>
         </p>
