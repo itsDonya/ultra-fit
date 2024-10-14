1cab6e7616510760c533a2df13650c3f23048b56
Donya <thisis2nya@gmail.com>
2024-10-15 01:50:07 +0330

    fix(pupils): reset Pupil's data before opening add pop-up

--- pages/pupils.vue
+++ pages/pupils.vue
@@ -7,27 +7,25 @@
       <div class="flex items-center justify-end gap-2">
         <!-- search -->
         <div
-          class="w-28 md:w-52 h-7 md:h-10 px-2.5 flex items-center justify-start gap-2 border rounded-lg"
-        >
+          class="w-28 md:w-52 h-7 md:h-10 px-2.5 flex items-center justify-start gap-2 border rounded-lg">
           <i-magnifying-glass class="text-neutral-300"></i-magnifying-glass>
 
           <input
             type="text"
             placeholder="جستجوی شاگرد"
-            class="w-full h-full text-[10px] md:text-sm text-neutral-700"
-          />
+            class="w-full h-full text-[10px] md:text-sm text-neutral-700" />
         </div>
 
         <!-- add modal -->
         <v-dialog>
           <template v-slot:activator="{ props: activatorProps }">
             <v-btn
+              rounded="lg"
               color="primary"
               variant="outlined"
               v-bind="activatorProps"
-              rounded="lg"
-              class="px-2.5 md:px-3.5 h-7 md:h-10 flex flex-center hover:bg-primary/5 transition-200"
-            >
+              @click="resetPupilData"
+              class="px-2.5 md:px-3.5 h-7 md:h-10 flex flex-center hover:bg-primary/5 transition-200">
               <i-plus-solid class="ml-1 text-sm text-primary"></i-plus-solid>
               <span class="text-[10px] md:text-sm text-primary">افزودن</span>
             </v-btn>
@@ -36,50 +34,43 @@
           <template v-slot:default="{ isActive }">
             <div class="w-80 p-4 mx-auto bg-white rounded-xl-tw">
               <div
-                class="w-full h-full flex flex-col items-start justify-start gap-4 py-2"
-              >
+                class="w-full h-full flex flex-col items-start justify-start gap-4 py-2">
                 <p class="text-sm text-neutral-600">
                   اطلاعات شاگرد جدید را وارد کنید
                 </p>
 
                 <div
-                  class="w-full flex flex-col items-start justify-start gap-2.5"
-                >
+                  class="w-full flex flex-col items-start justify-start gap-2.5">
                   <input
                     type="text"
                     placeholder="نام"
                     v-model="pupil.firstName"
-                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                  />
+                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
 
                   <input
                     type="text"
                     v-model="pupil.lastName"
                     placeholder="نام خانوادگی"
-                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                  />
+                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
 
                   <input
                     type="text"
                     v-model="pupil.username"
                     placeholder="نام کاربری"
-                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                  />
+                    class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
                 </div>
               </div>
 
               <div class="w-full flex items-center justify-center gap-2">
                 <button
                   :disabled="!validPupil"
-                  class="w-full h-11 bg-primary flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                >
+                  class="w-full h-11 bg-primary flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                   <span class="text-sm text-white">ثبت</span>
                 </button>
 
                 <button
                   @click="isActive.value = false"
-                  class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                >
+                  class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                   <span class="text-sm text-neutral-500">انصراف</span>
                 </button>
               </div>
@@ -122,66 +113,56 @@
                     <div
                       v-bind="{ ...props, ...activatorProps }"
                       @click="setPupilData(item)"
-                      class="size-5 md:size-9 text-sm lg:text-base hover:bg-yellow-300/10 flex flex-center rounded-full transition-200 cursor-pointer group"
-                    >
+                      class="size-5 md:size-9 text-sm lg:text-base hover:bg-yellow-300/10 flex flex-center rounded-full transition-200 cursor-pointer group">
                       <i-pen-solid
-                        class="text-yellow-300/80 group-hover:text-yellow-300 transition-200"
-                      ></i-pen-solid>
+                        class="text-yellow-300/80 group-hover:text-yellow-300 transition-200"></i-pen-solid>
                     </div>
                   </template>
 
                   <template v-slot:default="{ isActive }">
                     <div class="w-80 p-4 mx-auto bg-white rounded-xl-tw">
                       <div
-                        class="w-full h-full flex flex-col items-start justify-start gap-4 py-2"
-                      >
+                        class="w-full h-full flex flex-col items-start justify-start gap-4 py-2">
                         <p class="text-sm text-neutral-600">
                           اطلاعات جدید شاگرد را وارد کنید
                         </p>
 
                         <div
-                          class="w-full flex flex-col items-start justify-start gap-2.5"
-                        >
+                          class="w-full flex flex-col items-start justify-start gap-2.5">
                           <input
                             type="text"
                             placeholder="نام"
                             v-model="pupil.firstName"
-                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                          />
+                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
 
                           <input
                             type="text"
                             v-model="pupil.lastName"
                             placeholder="نام خانوادگی"
-                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                          />
+                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
 
                           <input
                             type="text"
                             v-model="pupil.username"
                             placeholder="نام کاربری"
-                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200"
-                          />
+                            class="w-full h-11 px-3 text-sm bg-inherit border border-neutral-950/15 focus-within:border-primary focus-within:ring-2 focus-within:ring-primary/10 rounded-lg transition-200" />
                         </div>
                       </div>
 
                       <div
-                        class="w-full flex items-center justify-center gap-2"
-                      >
+                        class="w-full flex items-center justify-center gap-2">
                         <button
                           :disabled="!validPupil"
-                          class="w-full h-11 bg-primary flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                        >
+                          class="w-full h-11 bg-primary flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                           <span class="text-sm text-white">ثبت</span>
                         </button>
 
                         <button
                           @click="
-                            restPupilData();
+                            resetPupilData();
                             isActive.value = false;
                           "
-                          class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                        >
+                          class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                           <span class="text-sm text-neutral-500">انصراف</span>
                         </button>
                       </div>
@@ -199,11 +180,9 @@
                     <div
                       v-bind="{ ...props, ...activatorProps }"
                       @click="openDeleteModal(item)"
-                      class="size-5 md:size-9 text-sm lg:text-base hover:bg-red-500/10 flex flex-center rounded-full transition-200 cursor-pointer group"
-                    >
+                      class="size-5 md:size-9 text-sm lg:text-base hover:bg-red-500/10 flex flex-center rounded-full transition-200 cursor-pointer group">
                       <i-trash-can-regular
-                        class="text-red-500/80 group-hover:text-red-500 transition-200"
-                      ></i-trash-can-regular>
+                        class="text-red-500/80 group-hover:text-red-500 transition-200"></i-trash-can-regular>
                     </div>
                   </template>
 
@@ -219,11 +198,9 @@
                       </p>
 
                       <div
-                        class="w-full flex items-center justify-center gap-2"
-                      >
+                        class="w-full flex items-center justify-center gap-2">
                         <button
-                          class="w-full h-11 bg-red-500 flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                        >
+                          class="w-full h-11 bg-red-500 flex flex-center rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                           <span class="text-sm text-white">حذف</span>
                         </button>
 
@@ -232,8 +209,7 @@
                             isActive.value = false;
                             closeDeleteModal();
                           "
-                          class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200"
-                        >
+                          class="w-full h-11 flex flex-center border rounded-lg hover:brightness-90 disabled:brightness-75 transition-200">
                           <span class="text-sm text-neutral-500">انصراف</span>
                         </button>
                       </div>
@@ -315,7 +291,7 @@ const setPupilData = (item) => {
   pupil.value.lastName = item.lastName;
   pupil.value.username = item.username;
 };
-const restPupilData = () => {
+const resetPupilData = () => {
   pupil.value.firstName = null;
   pupil.value.lastName = null;
   pupil.value.username = null;
