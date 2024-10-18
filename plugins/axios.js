3cbbafce57685dde97218ee5e31c0455dfc968dd
Donya <thisis2nya@gmail.com>
2024-10-18 17:47:12 +0330

    feat(chore): set default header for token & handle unauthorized error in axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { navigateTo } from "nuxt/app";
 // import { errorPersianMessage } from "../utils/errors";
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -10,22 +11,22 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide("axios", axios);
 
   // Config axios interceptors
-  // axios.interceptors.request.use(
-  //   function (config) {
-  //     // Set Authorization Token as Header
-  //     const AUTH_TOKEN = useCookie("TulipsTOKEN").value || "";
-  //     if (AUTH_TOKEN) {
-  //       config.headers["Authorization"] = "Bearer " + AUTH_TOKEN;
-  //     }
+  axios.interceptors.request.use(
+    function (config) {
+      // Set Authorization Token as Header
+      const AUTH_TOKEN = useCookie("ULTRA_TK").value || "";
+      if (AUTH_TOKEN) {
+        config.headers["Authorization"] = "Bearer " + AUTH_TOKEN;
+      }
 
-  //     // Do something before request is sent
-  //     return config;
-  //   },
-  //   function (error) {
-  //     // Do something with request error
-  //     return Promise.reject(error);
-  //   }
-  // );
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
 
   // Config axios interceptors
   axios.interceptors.response.use(
@@ -34,8 +35,16 @@ export default defineNuxtPlugin((nuxtApp) => {
       return response;
     },
     function (error) {
+      // handle network error
       if (error?.message == "Network Error") {
         nuxtApp.$toast.error("خطا در اتصال به شبکه");
+      }
+
+      // handle unauthorized error
+      if (error?.status == 401) {
+        nuxtApp.$toast.warning("لطفا ابتدا وارد شوید");
+        navigateTo("/login");
+        return Promise.reject();
       }
 
       Promise.reject(error);
