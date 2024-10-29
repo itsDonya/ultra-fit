5afabe9498b5ff1acbd33074105b952657771c58
Donya <thisis2nya@gmail.com>
2024-10-29 18:23:02 +0330

    feat(auth): login with otp code

--- pages/login.vue
+++ pages/login.vue
@@ -1,38 +1,91 @@
 <template>
-  <form
-    @keydown.enter="userLogin"
-    class="w-full flex flex-col items-center justify-center gap-4 sm:gap-8">
+  <form class="w-full flex flex-col items-center justify-center gap-4 sm:gap-8">
     <h1 class="sm:text-xl text-neutral-200">ورود به حساب کاربری</h1>
 
-    <div
-      class="w-full flex flex-col items-center justify-start gap-1.5 sm:gap-3">
-      <!-- username -->
-      <input
-        type="text"
-        maxlength="11"
-        placeholder="شماره موبایل"
-        v-model="data.username"
-        class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
+    <v-tabs fixed-tabs v-model="activeTab" color="light" class="w-full">
+      <v-tab value="mobile">ورود با رمز عبور</v-tab>
+      <v-tab value="otp">ورود با رمز یکبار مصرف</v-tab>
+    </v-tabs>
 
-      <!-- password -->
-      <input
-        type="password"
-        placeholder="رمز عبور"
-        v-model="data.password"
-        class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
-    </div>
+    <v-window v-model="activeTab" class="w-full">
+      <v-window-item value="mobile" class="w-full">
+        <div
+          class="w-full flex flex-col items-center justify-start gap-1.5 sm:gap-3">
+          <!-- username -->
+          <input
+            type="text"
+            maxlength="11"
+            placeholder="شماره موبایل"
+            v-model="data.username"
+            class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
+
+          <!-- password -->
+          <input
+            type="password"
+            placeholder="رمز عبور"
+            v-model="data.password"
+            class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
+
+          <!-- button -->
+          <v-btn
+            color="primary"
+            :loading="loading"
+            :disabled="!validData || loading"
+            @click.prevent="userLogin"
+            class="w-full h-9 sm:h-12 mt-4 rounded-lg sm:rounded-xl">
+            <span class="text-xs sm:text-base">ورود</span>
+          </v-btn>
+        </div>
+      </v-window-item>
+
+      <v-window-item value="otp" class="w-full">
+        <div
+          class="w-full flex flex-col items-center justify-start gap-1.5 sm:gap-3">
+          <v-window v-model="otpLevel" class="w-full">
+            <!-- mobile -->
+            <v-window-item value="mobile" class="w-full">
+              <input
+                type="text"
+                maxlength="11"
+                placeholder="شماره موبایل"
+                v-model="data.username"
+                class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
+
+              <!-- button -->
+              <v-btn
+                color="primary"
+                :loading="loading"
+                :disabled="!data.username || data.username.length != 11"
+                @click.prevent="sendOtp"
+                class="w-full h-9 sm:h-12 mt-4 rounded-lg sm:rounded-xl">
+                <span class="text-xs sm:text-base">ارسال کد یکبار مصرف</span>
+              </v-btn>
+            </v-window-item>
+
+            <!-- code -->
+            <v-window-item value="code" class="w-full">
+              <input
+                type="text"
+                placeholder="کد پیامک شده"
+                v-model="otpCode"
+                class="w-full h-9 sm:h-12 p-2 sm:p-4 text-xs sm:text-base text-white bg-neutral-200/20 border border-white/20 placeholder:text-white/40 focus:border-secondary rounded-lg sm:rounded-xl-tw transition-all duration-200" />
+
+              <!-- button -->
+              <v-btn
+                color="primary"
+                :loading="loading"
+                :disabled="!otpCode || otpCode.length != 4"
+                @click.prevent="verifyOtp"
+                class="w-full h-9 sm:h-12 mt-4 rounded-lg sm:rounded-xl">
+                <span class="text-xs sm:text-base">ورود به حساب</span>
+              </v-btn>
+            </v-window-item>
+          </v-window>
+        </div>
+      </v-window-item>
+    </v-window>
 
     <div class="w-full flex flex-col items-center justify-center gap-4">
-      <!-- button -->
-      <v-btn
-        color="primary"
-        :loading="loading"
-        :disabled="!validData || loading"
-        @click.prevent="userLogin"
-        class="w-full h-9 sm:h-12 rounded-lg sm:rounded-xl">
-        <span class="text-xs sm:text-base">ورود</span>
-      </v-btn>
-
       <!-- login -->
       <nuxt-link to="/register">
         <p class="text-xs sm:text-sm text-neutral-300">
@@ -52,7 +105,11 @@ definePageMeta({
 });
 
 // variables
+const userId = ref(null);
+const otpCode = ref(null);
 const loading = ref(false);
+const activeTab = ref("mobile");
+const otpLevel = ref("mobile");
 const nuxtApp = useNuxtApp();
 const data = ref({
   username: null,
@@ -115,4 +172,76 @@ const userLogin = async () => {
       loading.value = false;
     });
 };
+
+const sendOtp = async () => {
+  // check if login api is already called, prevent from calling again
+  if (loading.value) return;
+
+  loading.value = true;
+
+  await nuxtApp.$axios
+    .post("/User/LoginWithSMS", {
+      username: data.value.username,
+      role: "Coach",
+    })
+    .then((res) => {
+      console.log("res: ", res.data.result);
+      userId.value = res.data.result.userId;
+      otpLevel.value = "code";
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+      }
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
+
+const verifyOtp = async () => {
+  // check if login api is already called, prevent from calling again
+  if (loading.value) return;
+
+  loading.value = true;
+
+  await nuxtApp.$axios
+    .post("/User/GenerateUserToken", {
+      userId: userId.value,
+      otpCode: otpCode.value,
+    })
+    .then((res) => {
+      if (res.data.errorCode == "WrongOtp") {
+        nuxtApp.$toast.error("کد یکبار مصرف وارد شده صحیح نمی‌باشد");
+        otpCode.value = null;
+      } else {
+        nuxtApp.$toast.success("با موفقیت وارد شدید");
+
+        navigateTo("/");
+      }
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+      }
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
 </script>
+
+<style lang="scss">
+.v-tabs {
+  .v-btn {
+    border-radius: 10px 10px 0 0 !important;
+  }
+
+  .v-btn:not(.v-tab--selected) {
+    color: #a4d8fc80 !important;
+  }
+  .v-btn.v-tab--selected {
+    background-color: #a4d8fc20;
+  }
+}
+</style>
